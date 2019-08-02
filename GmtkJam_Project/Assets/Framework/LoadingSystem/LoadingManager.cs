75c8a6176f8ee3251993581f833e42727cf04e89
Rowen Kuiper <r0w3nkuiper@hotmail.com>
2019-08-02 20:40:51 +0200

    - added loadingsystem

---
+++ GmtkJam_Project/Assets/Framework/LoadingSystem/LoadingManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace r0w3ntje
+{
+    public class LoadingManager : Singleton<LoadingManager>
+    {
+        private void Start()
+        {
+            ToScene(1);
+        }
+
+        public void ToScene(int _sceneIndex)
+        {
+            StartCoroutine(LoadScene(_sceneIndex));
+        }
+
+        private IEnumerator LoadScene(int sceneIndex)
+        {
+            AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneIndex);
+            asyncOperation.allowSceneActivation = false;
+
+            while (!asyncOperation.isDone)
+            {
+                if (asyncOperation.progress >= 0.9f) break;
+                yield return null;
+            }
+
+            FadeManager.Instance().FadeWithAction(() => { asyncOperation.allowSceneActivation = true; });
+        }
+    }
+}
