2a9346184c126fbd5f1e1267268792cd2fcb3c94
Rowen Kuiper <r0w3nkuiper@hotmail.com>
2019-08-02 20:32:07 +0200

    - Added fadesystem

---
+++ GmtkJam_Project/Assets/Framework/FadeSystem/FadeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.Events;
+
+public class FadeManager : Singleton<FadeManager>
+{
+    [SerializeField] private Image fadeImage = null;
+
+    public void FadeWithAction(UnityAction action)
+    {
+        StartCoroutine(CoFadeWithAction(action));
+    }
+
+    private IEnumerator CoFadeWithAction(UnityAction action)
+    {
+        fadeImage.DOBlendableColor(new Color(1, 1, 1, 1), 0.25f);
+
+        yield return new WaitForSeconds(0.25f);
+        action?.Invoke();
+
+        fadeImage.DOBlendableColor(new Color(1, 1, 1, 0), 0.25f);
+    }
+}
