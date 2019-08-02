8b9b1e77f30fc59c59f3a609f1ce91dc7b47a9a4
Nighel Nijhuis <nighelnijhuis@gmail.com>
2019-08-03 01:01:33 +0200

    Player movement

---
+++ GmtkJam_Project/Assets/Content/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rb;
+    [SerializeField]
+    float movementSpeed = 100f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal"), // * movementSpeed 
+            verticalMovement = Input.GetAxis("Vertical"); // * movementSpeed
+
+        Vector3 normalize = new Vector3(horizontalMovement, rb.velocity.y, verticalMovement).normalized;
+
+        //Vector3.Normalize(new Vector3(horizontalMovement, rb.velocity.y, verticalMovement))
+
+        rb.AddForce(normalize * movementSpeed);
+    }
+}
