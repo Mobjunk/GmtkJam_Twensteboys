b375782b8379e3739b51d5b7b717366a35d5ae94
marc <marc@bekhuis.net>
2019-08-03 10:52:16 +0200

    Cinematic camera
    Movement werkt, alleen soms gaat hij langs het target heen maar weet niet hoe de transision van cinematic naar player follow is geplant

---
+++ GmtkJam_Project/Assets/Content/Scripts/CinematicCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicCamera : MonoBehaviour
+{
+    public Vector3 cinematicEndPosition;
+    public float speed;
+
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3((gameObject.transform.position.x - cinematicEndPosition.x) / speed * Time.fixedDeltaTime, (gameObject.transform.position.y - cinematicEndPosition.y) / speed * Time.fixedDeltaTime, (gameObject.transform.position.z - cinematicEndPosition.z) / speed * Time.fixedDeltaTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (gameObject.transform.position != cinematicEndPosition)
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - offset.x, gameObject.transform.position.y - offset.y, gameObject.transform.position.z - offset.z);
+        }
+    }
+}
