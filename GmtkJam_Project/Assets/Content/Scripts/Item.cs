dcefe7a2ec828caf3feedf2b02e90c051391287d
Noah <NoahPBrinkman@gmail.com>
2019-08-03 22:28:55 +0200

    Added Door and Key feature

---
+++ GmtkJam_Project/Assets/Content/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+	[SerializeField] private int itemID = 0;
+	public int ItemID => itemID;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		PlayerInventory pINV = other.GetComponent<PlayerInventory>();
+		if (pINV != null)
+		{
+			pINV.PickUpKey(this);
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ GmtkJam_Project/Assets/Content/Scripts/PlayerInventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+	[SerializeField]private List<Item> items = new List<Item>();
+	public List<Item> Items => items;
+
+	public void PickUpKey(Item pickedUpKey)
+	{
+		foreach(Item key in items)
+		{
+			if(key == pickedUpKey)
+			{
+				return;
+			}
+		}
+		items.Add(pickedUpKey);
+	}
+}
